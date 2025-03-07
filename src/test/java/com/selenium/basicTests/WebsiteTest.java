0a8d968ff66d0148936a815eccc75bfa990ce360
Sethu Madhav Guntha <sethumadhavguntha@gmail.com>
2025-03-07 10:18:57 +0530

    Basic Changes

--- src/test/java/com/selenium/basicTests/WebsiteTest.java
+++ src/test/java/com/selenium/basicTests/WebsiteTest.java
@@ -1,22 +1,13 @@
 package com.selenium.basicTests;
 
 import org.testng.annotations.Test;
-<<<<<<< HEAD
-=======
-import org.testng.annotations.Test;
->>>>>>> 9b54c65 (Website Sample Test)
-
 import com.selenium.utility.BrowserManager;
 
 public class WebsiteTest {
 	BrowserManager browserManagerObj = new BrowserManager();
 	@Test
 	public void testingYoutube() {
-<<<<<<< HEAD
 		browserManagerObj.lauchWebsiteByBrowser("chrome", "https://demoqa.com/");
-=======
-		browserManagerObj.lauchWebsiteByBrowser("safari", "https://demoqa.com/");
->>>>>>> 9b54c65 (Website Sample Test)
 		System.out.println("On demo page");
 		browserManagerObj.quitBrowser();
 	}
