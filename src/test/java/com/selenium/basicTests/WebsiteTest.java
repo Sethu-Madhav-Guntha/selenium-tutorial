a83cce9d0c0937a3e5b575e154d370adeb7d61f4
Sethu Madhav Guntha <sethumadhavguntha@gmail.com>
2025-03-07 10:15:10 +0530

    Started

--- src/test/java/com/selenium/basicTests/WebsiteTest.java
+++ src/test/java/com/selenium/basicTests/WebsiteTest.java
@@ -1,6 +1,10 @@
 package com.selenium.basicTests;
 
 import org.testng.annotations.Test;
+<<<<<<< HEAD
+=======
+import org.testng.annotations.Test;
+>>>>>>> 9b54c65 (Website Sample Test)
 
 import com.selenium.utility.BrowserManager;
 
@@ -8,7 +12,11 @@ public class WebsiteTest {
 	BrowserManager browserManagerObj = new BrowserManager();
 	@Test
 	public void testingYoutube() {
+<<<<<<< HEAD
 		browserManagerObj.lauchWebsiteByBrowser("chrome", "https://demoqa.com/");
+=======
+		browserManagerObj.lauchWebsiteByBrowser("safari", "https://demoqa.com/");
+>>>>>>> 9b54c65 (Website Sample Test)
 		System.out.println("On demo page");
 		browserManagerObj.quitBrowser();
 	}
